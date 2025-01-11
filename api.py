e2628d56f20146321f13d94633c0f7049367d651
Juan Pablo <41731415+Juanpgm@users.noreply.github.com>
2025-01-11 03:28:13 -0500

    Pude crear la petición get_maps de la API

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, File, UploadFile, Depends, HTTPException
+from fastapi import FastAPI, File, UploadFile, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 import json
 from database import Base, engine, SessionLocal
 from models import Map
+from geoalchemy2 import WKBElement
+from shapely.wkb import loads as to_shape
 
 # Crear tablas en la base de datos
 Base.metadata.create_all(bind=engine)
@@ -61,3 +63,28 @@ async def create_map(name: str, entity_name: str, file: UploadFile = File(...), 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
 
+@app.get("/maps/")
+def get_maps(name: str = Query(None, description="Name of the map to filter")):
+    try:
+        db: Session = SessionLocal()
+        if name:
+            maps = db.execute(text("SELECT id, name, entity_name, ST_AsGeoJSON(geom) as geom FROM maps WHERE name = :name"), {"name": name}).fetchall()
+        else:
+            maps = db.execute(text("SELECT id, name, entity_name, ST_AsGeoJSON(geom) as geom FROM maps")).fetchall()
+        
+        response_data = []
+
+        for map_item in maps:
+            map_dict = {
+                "id": map_item.id,
+                "name": map_item.name,
+                "entity_name": map_item.entity_name,
+                "geom": map_item.geom
+            }
+            response_data.append(map_dict)
+
+        return response_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        db.close()
